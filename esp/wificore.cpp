c4ff63ed309ac5ce2694b70db4ddd222c3e891d9
Dmitry Kotlyar <dm.kotlyar@yandex.ru>
2018-03-16 22:30:25 +0300

    Add OTA updates support

--- esp/wificore.cpp
+++ esp/wificore.cpp
@@ -7,6 +7,7 @@
 #include <ESP8266HTTPClient.h>
 #include <EEPROM.h>
 #include <ESP8266Ping.h>
+#include <ArduinoOTA.h>
 
 mactable_t mactable[MACTABLE_SIZE];
 
@@ -64,10 +65,38 @@ void wifiSetup() {
   server.on("/system/healthcheck", handleSystemHealthcheck);
   server.on("/system/changeServer", handleChangeServer);
   server.begin();
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start OTA update");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd OTA update");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA begined");
 }
 
 void wifiLoop() {
   server.handleClient();
+  ArduinoOTA.handle();
 
   if (wifiConnected()) {
     if (scanServer) {
